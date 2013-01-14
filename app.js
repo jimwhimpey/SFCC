e7dbc815c01af657fbe8da47c0f2ade05de52e92
Jim Whimpey <jimwhimpey@me.com>
2013-01-13 16:24:54 -0800

    Added sass support

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express'),
     exphbs  = require('express3-handlebars'),
+    sass    = require('node-sass'),
     app     = express();
 
 // Handlebars setup
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// Add SASS compiling
+app.configure(function () {
+	app.use(sass.middleware({
+		src: 'styles', 
+		dest: __dirname + '/public', 
+		debug: true,
+		output_style: 'compressed'
+	}));
+	app.use(express.static(__dirname + '/public'));
+});
+
 
 app.get('/', function(req, res){
 	res.render('home');
