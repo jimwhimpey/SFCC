b50ca2bdf71605161cba36d54193b7e40e500c12
jimwhimpey <jimwhimpey@me.com>
2013-01-24 21:14:59 -0800

    Added some NPM config

--- app.js
+++ app.js
@@ -45,5 +45,7 @@ app.get('/contact', function(req, res){
 });
 
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
