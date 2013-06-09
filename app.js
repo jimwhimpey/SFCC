bf3fa9c716bdebc0ec2e4b800da5f35fa3cc3d43
jimwhimpey <jimwhimpey@me.com>
2013-06-09 16:52:11 -0700

    Merged the rides and calendar pages. Upgraded to using the calendar API which is much much better. Better presentation and more info for calendar events and it actually works properly now which is nice. Some style changes too.

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var express = require('express'),
     Instagram = require('instagram-node-lib'),
     app     = express(),
     http    = require("http"),
+    https    = require("https"),
     parser  = require('xml2js').parseString,
-	  moment  = require('moment');
+    moment  = require('moment'),
+    urlParser = require('url');
 
 // Handlebars setup
 app.engine('handlebars', exphbs({	defaultLayout: 'main',
@@ -62,60 +64,61 @@ app.get('/contact', function(req, res){
 	res.render('contact', {'type': 'page'});
 });
 
-app.get('/calendar', function(req, response){
+app.get('/rides-calendar', function(req, response){
 
-	var options = {
-	  host: 'www.google.com',
-	  path: '/calendar/feeds/ktihar6uvc0lc1odtovk41fklo%40group.calendar.google.com/public/full',
-	  port: 80
-	};
+	var url = 'https://www.googleapis.com/calendar/v3/calendars/ktihar6uvc0lc1odtovk41fklo@group.calendar.google.com/events';
+	url += "?key=AIzaSyCDwjXV97tycQggoNhNjOwmaphpgoo39PA";
+	url += '&maxResults=12';
+	url += '&singleEvents=true';
+	url += '&orderBy=startTime';
 
-	function compare(a,b) {
-		if (a.timestamp < b.timestamp)
-			return -1;
-		if (a.timestamp > b.timestamp)
-			return 1;
-		return 0;
-	}
-
-	http.get(options, function(res) {
-
+	https.get(url, function(res) {
+	
 		var data = "",
 			calendar = [];
 
 		// Chunk the data together piece by piece as it gets returned
 		res.on("data", function (chunk) {data += chunk; });
-
-		// Onse all the data is returned
+		
+		// // Once all the data is returned
 		res.on("end", function () {
-
-			// Parse it with XML2JS
-			parser(data, function (err, result) {
-
-				// Loop trhough it
-				for (var i=0; i < result.feed.entry.length; i++) {
-
-					console.log("TEST");
-					console.log("result.feed.entry[i]['gd:when'][0]['$'].startTime", result.feed.entry[i]['gd:when'][0]['$'].startTime);
-
-					var time = moment(result.feed.entry[i]['gd:when'][0]['$'].startTime).subtract('hours', 7);
-
-					calendar.push({
-						"timestamp": time.format("X"),
-						"date": time.format("dddd, MMMM Do, h:mma"),
-						"title": result.feed.entry[i].title[0]._,
-						"where": result.feed.entry[i]['gd:where'][0]['$'].valueString
-					});
-				};
-
-				calendar.sort(compare);
-
-				response.render('calendar', {'type': 'page', 'calendar': calendar});
-
-			});
-
+			
+			// Parse the string into JSON
+			data = JSON.parse(data);
+			
+			// Loop through it
+			for (var i=0; i < data.items.length; i++) {
+	
+				var time, timeFormatted, timeRelative;
+				
+				if (typeof data.items[i].start.dateTime == "undefined") {
+					time = moment(data.items[i].start.date);
+					timeFormatted = time.format("dddd, MMMM Do");
+				} else {
+					time = moment(data.items[i].start.dateTime);
+					timeFormatted = time.format("dddd, MMMM Do, h:mma");
+				}
+				
+				timeRelative = time.fromNow();
+				
+				var recuring = (typeof data.items[i].recurringEventId == "undefined") ? false : true;
+	
+				calendar.push({
+					"timestamp": time.format("X"),
+					"date": timeFormatted,
+					"dateRelative": (timeRelative == "in a day") ? "tomorrow" : timeRelative,
+					"title": data.items[i].summary,
+					"where": data.items[i].location,
+					"description": data.items[i].description,
+					"recuring": recuring
+				});
+				
+			};
+			
+			response.render('rides-calendar', {'type': 'page', 'calendar': calendar});
+			
 		});
-
+		
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
 	});
