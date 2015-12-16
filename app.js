93e31e6d79fa48344c6cf9fc322d3ddf0cb11a51
Jim Whimpey <whimpey@yahoo-inc.com>
2015-12-16 10:00:13 -0800

    Removed the pulling in of the calendar because it was crashing the server on heroku cedar-14

--- app.js
+++ app.js
@@ -74,74 +74,7 @@ app.get('/docs', function(req, res){
 
 app.get('/rides-calendar', function(req, response){
 
-	var url = 'https://www.googleapis.com/calendar/v3/calendars/ktihar6uvc0lc1odtovk41fklo@group.calendar.google.com/events';
-	url += "?key=AIzaSyCDwjXV97tycQggoNhNjOwmaphpgoo39PA";
-	url += '&maxResults=12';
-	url += '&singleEvents=true';
-	url += '&orderBy=startTime';
-	url += '&timeMin=' + moment().format();
-
-	https.get(url, function(res) {
-	
-		var data = "",
-			calendar = [];
-
-		// Chunk the data together piece by piece as it gets returned
-		res.on("data", function (chunk) {data += chunk; });
-		
-		// // Once all the data is returned
-		res.on("end", function () {
-			
-			// Parse the string into JSON
-			data = JSON.parse(data);
-			
-			// Loop through it
-			for (var i=0; i < data.items.length; i++) {
-	
-				var time, timeFormatted, timeRelative;
-				
-				if (typeof data.items[i].start.dateTime == "undefined") {
-					time = moment(data.items[i].start.date);
-					timeFormatted = time.format("dddd, MMMM Do");
-				} else {
-					time = moment(data.items[i].start.dateTime);
-					timeFormatted = time.format("dddd, MMMM Do, h:mma");
-				}
-				
-				timeRelative = time.fromNow();
-				
-				var recuring = (typeof data.items[i].recurringEventId == "undefined") ? false : true;
-				
-				// Convert new lines to paragraphs
-				var description = "";
-				if (typeof data.items[i].description !== 'undefined') {
-					description = data.items[i].description.replace(/\n{2,}/gim, "</p><p>");
-					description = description.replace(/\n/gim, '<br>');
-					description = "<p>" + description + "</p>";
-				}
-				
-				// Convert URLs into proper links
-				description = replaceURLWithHTMLLinks(description);
-				
-				calendar.push({
-					"timestamp": time.format("X"),
-					"date": timeFormatted,
-					"dateRelative": (timeRelative == "in a day") ? "tomorrow" : timeRelative,
-					"title": data.items[i].summary,
-					"where": data.items[i].location,
-					"description": description,
-					"recuring": recuring
-				});
-				
-			};
-			
-			response.render('rides-calendar', {'type': 'page', 'calendar': calendar});
-			
-		});
-		
-	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
-	});
+	response.render('rides-calendar', {'type': 'page', 'calendar': {}});
 
 });
 
@@ -149,7 +82,7 @@ app.get('/rides-calendar', function(req, response){
 
 function replaceURLWithHTMLLinks(text) {
 	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-	return text.replace(exp,"<a href='$1'>$1</a>"); 
+	return text.replace(exp,"<a href='$1'>$1</a>");
 }
 
 var port = process.env.PORT || 5000;
